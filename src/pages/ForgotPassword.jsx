cf260b7b6e699b9cf1f5995743f6d56dc05ae4ad
wilsonTec <wilson.garcia@tecsup.edu.pe>
2022-06-17 11:18:58 -0500

    Proyecto react con routers

---
+++ src/pages/ForgotPassword.jsx
@@ -0,0 +1,33 @@
+import {Link} from 'react-router-dom';
+
+function ForgotPassword(){
+    return(
+        <div class="container">
+                        <div class="row justify-content-center">
+                            <div class="col-lg-5">
+                                <div class="card shadow-lg border-0 rounded-lg mt-5">
+                                    <div class="card-header"><h3 class="text-center font-weight-light my-4">Password Recovery</h3></div>
+                                    <div class="card-body">
+                                        <div class="small mb-3 text-muted">Enter your email address and we will send you a link to reset your password.</div>
+                                        <form>
+                                            <div class="form-floating mb-3">
+                                                <input class="form-control" id="inputEmail" type="email" placeholder="name@example.com" />
+                                                <label for="inputEmail">Email address</label>
+                                            </div>
+                                            <div class="d-flex align-items-center justify-content-between mt-4 mb-0">
+                                                <Link to="/login">Retornar a Login</Link>
+                                                <a class="btn btn-primary" href="login.html">Reset Password</a>
+                                            </div>
+                                        </form>
+                                    </div>
+                                    <div class="card-footer text-center py-3">
+                                        <div class="small"><Link to="/register">No tienes cuenta, registrate!</Link></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+        </div>
+    )
+}
+
+export default ForgotPassword;
